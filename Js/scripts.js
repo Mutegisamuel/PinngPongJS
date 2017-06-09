67d41828c60660f818744121e944838c50e52423
mutegisamuel <mutegisamuel0@gmail.com>
2017-06-09 17:33:11 +0300

    new site that determines the numbers divisible by 3,5 & 15 by printing out ping,pong and ping-pong respectively

---
+++ Js/scripts.js
@@ -0,0 +1,30 @@
+
+function pingPong(number) {  
+  var result = [];
+  for (var i = 1; i <= number; i++){
+    if ((i % 15) === 0){
+      result.push("Ping-Pong");
+    } else if ((i % 5) === 0){
+      result.push("Pong");
+    } else if ((i % 3) === 0){
+      result.push("Ping");
+    } else {
+      result.push(i)
+    }
+  }
+  
+  return result;   
+
+}
+
+$(document).ready(function() {
+  $("form#pingPongForm").submit(function(event) {
+    event.preventDefault();
+
+    var input = parseInt($("input#number").val());
+    var result = pingPong(input);
+    result.forEach(function(number) {
+      $("ul#result").append("<li>"+number+"</li>");
+    });
+  });
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+####ping-pong, js project.
+        technologies used:bootstrap,html,css,javascript,jquery 3.2.1
+LINCENCE: MIT
+        AUTHOR: Mutegi Samuel.  
